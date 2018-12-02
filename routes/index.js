50f5402e36f54cd49230e49ad9ab38498cffb705
WANGXuecong <blold344@gmail.com>
2018-12-02 23:35:05 +1100

    test deploy

--- routes/index.js
+++ routes/index.js
@@ -37,9 +37,9 @@ exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.home);
 
-	// app.get('/mainHeader', function (req, res, next) {
-	// 	res.render('mainHeader.ejs', { page_title: 'about' });
-	// });
+	app.get('/test', function (req, res, next) {
+		res.send('Test keystoneRuning!', { page_title: 'about' });
+	});
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
