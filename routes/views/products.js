5abccad510313a0b9e691714f9b69ff6fa1eea58
WANGXuecong <blold344@gmail.com>
2019-01-22 22:08:46 +1100

    test

--- routes/views/products.js
+++ routes/views/products.js
@@ -9,8 +9,6 @@ exports = module.exports = function (req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
 	locals.section = 'products';
 	view.query('products', Products.model.find({}).sort({ sortOrder: 1 }));
 	view.on('init', function (next) {
